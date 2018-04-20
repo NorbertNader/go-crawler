027e8385e9bad4d255eec3a0b8d5650b3fbabc8b
Norbert Nader <Norbert.Nader@gmail.com>
2018-04-20 23:33:21 +0200

    Update save-file.go

--- hello-world/hello-world/save-file.go
+++ hello-world/hello-world/save-file.go
@@ -9,9 +9,9 @@ func main() {
 	f, err := os.Create("tmp/some.txt")
 
 	if err != nil {
-		fmt.Println(err.Error() + " we hade some shit happen")
+		fmt.Println(err.Error() + " ooops")
 	} else {
 		defer f.Close()
-		fmt.Println("file created bitch!")
+		fmt.Println("file created!")
 	}
 }
